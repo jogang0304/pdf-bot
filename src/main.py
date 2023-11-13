b615bdecd9d14e9cea04202bacf4194c0bd873c0
jogang <jogang0304@gmail.com>
2023-11-13 21:01:34 +0300

    сделал webhook

--- src/main.py
+++ src/main.py
@@ -3,8 +3,22 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 from aiogram.utils.markdown import hbold
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 
 from src.pdf_form import router as pdf_router
+
+WEB_SERVER_HOST = "95.140.158.148"
+# Port for incoming request from reverse proxy. Should be any available port
+WEB_SERVER_PORT = 8088
+
+# Path to webhook route, on which Telegram will send requests
+WEBHOOK_PATH = "/webhook"
+# Secret key to validate requests from Telegram (optional)
+WEBHOOK_SECRET = "maIsdkf*(1t"
+# Base URL for webhook will be used to generate webhook URL for Telegram,
+# in this example it is used public DNS with HTTPS support
+BASE_WEBHOOK_URL = "https://aiogram.dev/"
 
 dp = Dispatcher()
 
@@ -15,7 +29,22 @@ async def command_start_handler(message: Message) -> None:
         await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
 
+async def on_startup(bot: Bot) -> None:
+    await bot.set_webhook(
+        f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET
+    )
+
+
 async def main(TOKEN: str) -> None:
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
     dp.include_router(pdf_router)
-    await dp.start_polling(bot)
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    app = web.Application()
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    # await dp.start_polling(bot)
